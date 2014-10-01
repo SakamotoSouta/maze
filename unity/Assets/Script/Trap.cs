351c4eb89a8b585521b4a465bdfca49bdc3da968
Souta Sakamoto <tkintern13@gmail.com>
2014-10-01 16:44:34 +0900

    トラップの実装
    あたるとスタート地点へ戻るオブジェクトの作成＆配置を行った。

---
+++ unity/Assets/Script/Trap.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+	// 触れた瞬間
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "Ball") {
+			GameObject respawn = GameObject.FindWithTag("Respawn");
+			other.gameObject.transform.position = respawn.transform.position;
+		}
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+}
